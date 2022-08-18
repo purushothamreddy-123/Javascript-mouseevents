696cae5375b97ad862ec368392a1b2b9d1305f16
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-18 15:14:07 +0530

    Javascript mouseevents

---
+++ mouseevents.js
@@ -0,0 +1,12 @@
+function dpr1(a){
+    a .style .color="red";
+    a . style . backgroundColor="yellow";
+    a . innerHTML="mouseover";
+
+}
+function dpr2(b){
+    b .style .color="red";
+    b . style . backgroundColor="blue";
+    b . innerHTML="mouseout";
+
+}
